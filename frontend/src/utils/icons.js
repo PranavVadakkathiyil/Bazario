bf4129d1416eac4c746ac7e64d9340896576f470
Pranav Vadakkathiyil <vpranav0801@gmail.com>
2025-01-01 22:19:57 +0530

    Start Frontend

---
+++ frontend/src/utils/icons.js
@@ -0,0 +1,10 @@
+// Exporting frequenty using react-icon from react-icon package
+
+// Search logo
+export { GoSearch } from "react-icons/go";
+// Profile logo
+export { CgProfile } from "react-icons/cg";
+// Heart logo
+export { CiHeart } from "react-icons/ci";
+// Bag logo
+export { MdOutlineShoppingBag } from "react-icons/md";
